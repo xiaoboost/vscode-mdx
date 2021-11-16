fc4ecac62efa3550f6bfd142a01e0e52211b39c9
xiaoboost <xiaoboost@qq.com>
2021-11-17 02:15:28 +0800

    feat: add @types/mdx-js__react for internal packages

--- packages/language-internal/scripts/build.js
+++ packages/language-internal/scripts/build.js
@@ -2,20 +2,17 @@ const path = require('path');
 const fs = require('fs');
 const Glob = require('fast-glob');
 const outDir = path.resolve(__dirname, '../dist');
-const libs = ['@types/node', '@types/react', '@types/mdx-js__react'];
+const libs = ['@types/node', '@types/react', '@types/mdx-js__react', 'typescript'];
 
 async function writeData() {
   const files = {};
   const rootPath = path.resolve(__dirname, '..');
-  const globOption = {
+  const dtsFiles = await Glob(`node_modules/{${libs.join(',')}}/**/{*.d.ts,package.json}`, {
     cwd: rootPath,
-  };
-  const dtsFiles = await Glob(`node_modules/{${libs.join(',')}}/**/{*.d.ts,package.json}`, globOption);
-  const tsDtsFiles = await Glob('node_modules/typescript/**/{lib.*.d.ts,package.json}', globOption);
+  });
 
-  await Promise.all((dtsFiles.concat(tsDtsFiles).map(async (name) => {
-    const content = await fs.promises.readFile(path.join(rootPath, name), 'utf-8');
-    files[name] = content;
+  await Promise.all((dtsFiles.map(async (name) => {
+    files[name] = await fs.promises.readFile(path.join(rootPath, name), 'utf-8');
   })));
 
   await fs.promises.writeFile(path.join(outDir, 'index.json'), JSON.stringify(files));
